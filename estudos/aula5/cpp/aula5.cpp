e73087cd017209a195a2bd8ca337fb68459c299c
André Victor <andre899120@gmail.com>
2025-06-03 21:52:38 -0300

    aula 5 concluida

---
+++ estudos/aula5/cpp/aula5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define canal cout << "Canal Fessor Bruno\n\n"
+
+int main(){
+	
+	int vidas = 3, tiros = 500, life = 100;
+	
+	cout << pi << "\n\n";
+	canal;
+	
+	/*
+	int vidas;
+	int tiros;
+	int life;
+	
+    int vidas; int tiros; int life;
+	*/
+	
+	return 0;
+}
